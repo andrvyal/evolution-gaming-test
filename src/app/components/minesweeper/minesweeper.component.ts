9150d14a1be1350949079ef7e1cc7a34c5217858
Andrey Vyal <andr.vyal@gmail.com>
2020-09-24 23:34:33 +0300

    Improved bot algorithm

--- src/app/components/minesweeper/minesweeper.component.ts
+++ src/app/components/minesweeper/minesweeper.component.ts
@@ -110,6 +110,51 @@ export class MinesweeperComponent implements OnInit {
     return unknownNeighbors;
   }
 
+  private hasCell(cells: Array<MinesweeperCellCoordinates>, targetCell: MinesweeperCellCoordinates): boolean {
+    for (const cell of cells) {
+      if (this.isCellEqual(cell, targetCell)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private isCellEqual(cell1: MinesweeperCellCoordinates, cell2: MinesweeperCellCoordinates): boolean {
+    return (
+      cell1.rowIndex === cell2.rowIndex &&
+      cell1.colIndex === cell2.colIndex
+    );
+  }
+
+  private isGroupEqual(group1: MinesweeperCellGroup, group2: MinesweeperCellGroup): boolean {
+    if (group1.cells.length !== group1.cells.length) {
+      return false;
+    }
+
+    for (const cell of group1.cells) {
+      if (!this.hasCell(group2.cells, cell)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private isGroupParentChild(parent: MinesweeperCellGroup, child: MinesweeperCellGroup): boolean {
+    if (parent.cells.length <= child.cells.length) {
+      return false;
+    }
+
+    for (const cell of child.cells) {
+      if (!this.hasCell(parent.cells, cell)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   private nextMove(): void {
     setTimeout(() => {
       let flagsSet: boolean;
@@ -167,11 +212,16 @@ export class MinesweeperComponent implements OnInit {
       }
 
       if (this.minesweeperService.isOpen(rowIndex, colIndex)) {
+        const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
+
+        if (!mines) {
+          return;
+        }
+
         const unknownAround: Array<MinesweeperCellCoordinates> = this.getUnknownAround(rowIndex, colIndex);
 
         if (unknownAround.length) {
           const flagsAround: number = this.getFlagsAround(rowIndex, colIndex);
-          const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
 
           if (flagsAround === mines) {
             selected = unknownAround[0];
@@ -179,6 +229,55 @@ export class MinesweeperComponent implements OnInit {
         }
       }
     });
+
+    if (!selected) {
+      const groups: Array<MinesweeperCellGroup> = [];
+
+      this.minesweeperService.forEach((rowIndex: number, colIndex: number) => {
+        if (this.minesweeperService.isOpen(rowIndex, colIndex)) {
+          const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
+
+          if (!mines) {
+            return;
+          }
+
+          const unknownAround: Array<MinesweeperCellCoordinates> = this.getUnknownAround(rowIndex, colIndex);
+
+          if (unknownAround.length) {
+            const flagsAround: number = this.getFlagsAround(rowIndex, colIndex);
+            const unknownMines: number = mines - flagsAround;
+
+            if (unknownMines) {
+              groups.push({
+                cells: unknownAround,
+                mines: unknownMines,
+              });
+            }
+          }
+        }
+      });
+
+      for (const group1 of groups) {
+        if (selected) {
+          break;
+        }
+
+        for (const group2 of groups) {
+          if (this.isGroupEqual(group1, group2)) {
+            continue;
+          }
+
+          if (this.isGroupParentChild(group1, group2)) {
+            const difference: MinesweeperCellGroup = this.subtractGroup(group1, group2);
+
+            if (difference.mines === 0) {
+              selected = difference.cells[0];
+              break;
+            }
+          }
+        }
+      }
+    }
 
     if (!selected) {
       this.minesweeperService.forEach((rowIndex: number, colIndex: number) => {
@@ -209,14 +308,48 @@ export class MinesweeperComponent implements OnInit {
 
   private setFlags(): boolean {
     let flagsSet = false;
+
+    this.minesweeperService.forEach((rowIndex: number, colIndex: number) => {
+      if (this.minesweeperService.isOpen(rowIndex, colIndex)) {
+        const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
+
+        if (!mines) {
+          return;
+        }
+
+        const unknownAround: Array<MinesweeperCellCoordinates> = this.getUnknownAround(rowIndex, colIndex);
+
+        if (unknownAround.length) {
+          const flagsAround: number = this.getFlagsAround(rowIndex, colIndex);
+          const unknownMines: number = mines - flagsAround;
+
+          if (unknownMines) {
+            if (unknownAround.length === unknownMines) {
+              for (const cell of unknownAround) {
+                if (!this.minesweeperService.isFlagged(cell.rowIndex, cell.colIndex)) {
+                  this.minesweeperService.toggleFlag(cell.rowIndex, cell.colIndex);
+                  flagsSet = true;
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+
     const groups: Array<MinesweeperCellGroup> = [];
 
     this.minesweeperService.forEach((rowIndex: number, colIndex: number) => {
       if (this.minesweeperService.isOpen(rowIndex, colIndex)) {
+        const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
+
+        if (!mines) {
+          return;
+        }
+
         const unknownAround: Array<MinesweeperCellCoordinates> = this.getUnknownAround(rowIndex, colIndex);
 
         if (unknownAround.length) {
-          const mines: number = this.minesweeperService.getValue(rowIndex, colIndex);
           const flagsAround: number = this.getFlagsAround(rowIndex, colIndex);
           const unknownMines: number = mines - flagsAround;
 
@@ -230,12 +363,18 @@ export class MinesweeperComponent implements OnInit {
       }
     });
 
-    for (const group of groups) {
-      if (group.cells.length === group.mines) {
-        for (const cell of group.cells) {
-          if (!this.minesweeperService.isFlagged(cell.rowIndex, cell.colIndex)) {
-            this.minesweeperService.toggleFlag(cell.rowIndex, cell.colIndex);
-            flagsSet = true;
+    for (const group1 of groups) {
+      for (const group2 of groups) {
+        if (this.isGroupParentChild(group1, group2)) {
+          const difference: MinesweeperCellGroup = this.subtractGroup(group1, group2);
+
+          if (difference.mines === difference.cells.length) {
+            for (const cell of difference.cells) {
+              if (!this.minesweeperService.isFlagged(cell.rowIndex, cell.colIndex)) {
+                this.minesweeperService.toggleFlag(cell.rowIndex, cell.colIndex);
+                flagsSet = true;
+              }
+            }
           }
         }
       }
@@ -255,5 +394,20 @@ export class MinesweeperComponent implements OnInit {
     } finally {
       this.spinnerService.stop();
     }
+  }
+
+  private subtractGroup(from: MinesweeperCellGroup, minus: MinesweeperCellGroup): MinesweeperCellGroup {
+    const difference: MinesweeperCellGroup = {
+      cells: [],
+      mines: from.mines - minus.mines,
+    };
+
+    for (const cell of from.cells) {
+      if (!this.hasCell(minus.cells, cell)) {
+        difference.cells.push(cell);
+      }
+    }
+
+    return difference;
   }
 }
